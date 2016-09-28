95baba12d1214a1e76f6a3c7c3cd1b4bf2aac0f8
yuyingliu331 <liuyuying90@live.cn>
2016-09-28 12:16:56 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+
+
+app.use(bodyParser.json());
+app.set('port', process.env.PORT || 8080);
+app.use(cors()); // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.use(express.static('www')); // Our Ionic app build is in the www folder (kept up-to-date by the Ionic CLI using 'ionic serve')
+
+
+// Initialize the app.
+app.listen(app.get('port'), function () {
+console.log("You're a wizard, Harry. I'm a what? Yes, a wizard, on port", app.get('port'));
+});
