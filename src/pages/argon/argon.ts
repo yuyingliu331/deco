f664cb1f80536e99a57c92b8c9310caa8a4d8438
cloworm <chloe.echikson@gmail.com>
2016-10-04 10:41:29 -0400

    added argon tutorial script

--- src/pages/argon/argon.ts
+++ src/pages/argon/argon.ts
@@ -4,6 +4,7 @@ import { NavController, NavParams } from 'ionic-angular';
 @Component({
   templateUrl: 'argon.html'
 })
+
 export class ArgonPage {
 
   constructor(public navCtrl: NavController, params: NavParams){
