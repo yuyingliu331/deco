a96b82c3249ecc93f44a07437a093e1c50532cc0
cloworm <chloe.echikson@gmail.com>
2016-10-06 14:39:32 -0400

    installed wikitude and configured the plugin

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -88,6 +88,14 @@ module.exports = [
             "cordova.plugins.Keyboard"
         ],
         "runs": true
+    },
+    {
+        "id": "com.wikitude.phonegap.WikitudePlugin.WikitudePlugin",
+        "file": "plugins/com.wikitude.phonegap.WikitudePlugin/www/WikitudePlugin.js",
+        "pluginId": "com.wikitude.phonegap.WikitudePlugin",
+        "clobbers": [
+            "WikitudePlugin"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -101,7 +109,8 @@ module.exports.metadata =
     "cordova-plugin-splashscreen": "4.0.0",
     "cordova-plugin-statusbar": "2.2.0",
     "cordova-plugin-whitelist": "1.3.0",
-    "ionic-plugin-keyboard": "2.2.1"
+    "ionic-plugin-keyboard": "2.2.1",
+    "com.wikitude.phonegap.WikitudePlugin": "5.3.0"
 };
 // BOTTOM OF METADATA
 });
