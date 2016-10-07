0a1c676baca9bffda5f2a2a33676a7fbe9cfff9a
Johanna Altmann <tintin301@gmail.com>
2016-10-07 12:49:47 -0400

    remove migration for Likes primary key, add primary key creation to old migration file to be re-migrated

--- server/migrations/20160929142849-create-likes.js
+++ server/migrations/20160929142849-create-likes.js
@@ -2,6 +2,12 @@
 module.exports = {
   up: function(queryInterface, Sequelize) {
     return queryInterface.createTable('Likes', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
      productId: {
       type: Sequelize.INTEGER,
       references: {
