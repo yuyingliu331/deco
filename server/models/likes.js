2ccea7e11c3158cd44771a2f8ea69de92654f094
josie11 <tintin301@gmail.com>
2016-10-09 12:38:47 -0400

    Update likes.js

--- server/models/likes.js
+++ server/models/likes.js
@@ -5,7 +5,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
-      type: Sequelize.INTEGER
+      type: DataTypes.INTEGER
     },
     productId: {
       type: DataTypes.INTEGER,
