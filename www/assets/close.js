91e904af04cf9ab31e153758b68ed3a05e9adc07
cloworm <chloe.echikson@gmail.com>
2016-10-12 12:40:58 -0400

    moved logo down and removed close buttons

--- www/assets/close.js
+++ www/assets/close.js
@@ -1,6 +1,10 @@
 var ref = window.open(encodeURI(url), '_blank', options);
 ref.addEventListener('loadstop', function(event) {
-    if (event.url.match("mobile/close")) {
-        ref.close();
-    }
+  if (event.url.match("mobile/close")) {
+    ref.close();
+  }
 });
+
+function close() {
+  window.close();
+}
