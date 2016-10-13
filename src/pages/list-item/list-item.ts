7c9e614841aabad2bb57b52d46a6d594d3415afe
yuyingliu331 <liuyuying90@live.cn>
2016-10-13 16:08:58 -0400

    Update list-item.ts

--- src/pages/list-item/list-item.ts
+++ src/pages/list-item/list-item.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 import { ProductDetailPage } from '../product/product-detail';
 import { WishlistService } from '../../providers/wishlist-service';
 
